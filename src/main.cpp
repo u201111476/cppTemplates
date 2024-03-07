722a30ae3c94cc39a5611ac8dbc30998b0f74b11
WeiJun <promise_faith@163.com>
2024-03-07 22:03:03 +0800

    feat:初始化windows测试项目

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <complex>
+
+using std::cout;
+using std::endl;
+
+template<typename T>
+T max(T a, T b)
+{
+    cout << typeid(T).name() << endl;
+    return a < b ? b : a;
+}
+
+int main()
+{
+    cout<<::max(1, 2)<<endl;
+    cout << ::max(3.0, 4.0) << endl;
+    std::string str1 = "mathmatics";
+    std::string str2 = "math";
+    cout << ::max(str1, str2) << endl;
+    std::complex<float> c1, c2;
+    //max(c1, c2);        //报错，对重载函数的调用不明确
+    return 0;
+}
